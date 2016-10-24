be20e659d86ea525c9e9a18f653ceb104386e995
Joscelyn Jean <joscelyn.jean@gmail.com>
2016-10-24 13:18:20 -0400

    Switch from sass to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { TranslateService } from 'ng2-translate';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.sass'],
+  styleUrls: ['./project.component.scss'],
   providers : [
     ProjectService
   ]
