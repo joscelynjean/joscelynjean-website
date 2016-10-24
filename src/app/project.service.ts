7219763036955ba5a5ce09501f4d01599ddd5256
Joscelyn Jean <joscelyn.jean@gmail.com>
2016-10-23 22:15:00 -0400

    Add project service

---
+++ src/app/project.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Response, Jsonp } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+export class Project {
+  name : string;
+}
+
+@Injectable()
+export class ProjectService {
+
+  constructor(private jsonp: Jsonp) { }
+
+  getProjects() {
+    var url = "http://api.github.com/users/joscelynjean/repos?callback=JSONP_CALLBACK";
+    return this.jsonp
+      .get(url)
+      .toPromise()
+      .then((response) => response.json().data)
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+}
---
+++ src/app/project/project.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Project, ProjectService } from '../project.service';
+import { Http, Response, Jsonp, JsonpModule } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.sass'],
+  providers : [
+    ProjectService
+  ]
+})
+export class ProjectComponent implements OnInit {
+
+  projects: Promise<Project[]>;
+
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    this.projects = this.projectService.getProjects();
+  }
+
+}
