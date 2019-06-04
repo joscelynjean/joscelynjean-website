ce7bab8924c3d47cdbf536a23aa0a4574af833e0
Joscelyn Jean <joscelyn.jean@gmail.com>
2019-06-03 20:32:52 -0400

    add twitter + adjust border focus on button

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule
   ],
-  exports: [MatCardModule, MatButtonModule]
+  exports: [MatCardModule, MatButtonModule, MatDividerModule]
 })
 export class AppMaterialModule { }
