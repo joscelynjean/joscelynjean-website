91cba62d603628bde48e3c21aed4a2c44d514c59
Joscelyn Jean <joscelyn.jean@gmail.com>
2019-07-26 21:21:26 -0400

    UT on header

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -8,9 +8,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +22,39 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should detect mobile if window width smaller than 756px', () => {
+    const windowSpy = spyOnProperty(window, 'innerWidth').and.returnValue(755);
+    component.ngOnInit();
+
+    expect(windowSpy).toHaveBeenCalled();
+    expect(window.innerWidth).toBe(755);
+    expect(component.isMobile).toBeTruthy();
+  });
+
+  it('should toggle menu when clicking on the hamburger icon in mobile mode', () => {
+
+    // Resize to mobile
+    component.onResize({ target: { innerWidth: 755 } });
+    expect(component.isMobile).toBeTruthy();
+    expect(component.showMenu).toBeFalsy();
+
+    // Should show the menu on first click
+    component.onMenuClick();
+    expect(component.showMenu).toBeTruthy();
+
+    // Should hide the menu on second click
+    component.onMenuClick();
+    expect(component.showMenu).toBeFalsy();
+
+    // Resize to desktop
+    component.onResize({ target: { innerWidth: 756 } });
+    expect(component.isMobile).toBeFalsy();
+    expect(component.showMenu).toBeTruthy();
+
+    // Click on the menu shouldn't affect visibility
+    component.onMenuClick();
+    expect(component.showMenu).toBeTruthy();
+  });
+
 });
