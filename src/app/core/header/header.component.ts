2fe304e5d725ae574823f467749fc77546e4639d
Joscelyn Jean <joscelyn.jean@gmail.com>
2019-07-26 20:26:23 -0400

    Menu is now compatible with mobile

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('menuItems')
+  menuItems: ElementRef<HTMLUListElement>;
+
+  showMenu = true;
+  isMobile = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.isMobile = this.isSmallWidth(window.innerWidth);
+    if (this.isMobile) {
+      this.showMenu = false;
+    }
   }
 
+  private isSmallWidth(width: number): boolean {
+    return width < 756;
+  }
+
+  onMenuClick() {
+    this.showMenu = !this.isMobile || !this.showMenu;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    const wasMobile = this.isMobile;
+    this.isMobile = this.isSmallWidth(event.target.innerWidth);
+
+    if (this.isMobile !== wasMobile) {
+      this.showMenu = !this.isMobile;
+    }
+  }
 }
